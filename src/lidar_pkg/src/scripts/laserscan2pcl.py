6f43dff51a3670f39a934323a5fcd61c596ec84b
yunusstalha <yunus.erzurumlu@metu.edu.tr>
2023-12-10 15:15:27 +0300

    states added for mapping

--- src/lidar_pkg/src/scripts/laserscan2pcl.py
+++ src/lidar_pkg/src/scripts/laserscan2pcl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import LaserScan, PointCloud2
+from std_msgs.msg import String  # Import String message type for state control
 import laser_geometry
 import tf
 
@@ -9,20 +10,33 @@ class LaserScanToPointCloud:
         self.node_name = "laser_scan_to_point_cloud"
         rospy.init_node(self.node_name)
 
-        self.laser_sub = rospy.Subscriber("/scan_filtered", LaserScan, self.laser_callback)
+        self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
         self.cloud_pub = rospy.Publisher("/cloud", PointCloud2, queue_size=10)
+        self.state_sub = rospy.Subscriber("/state_control", String, self.state_callback)  # State control subscriber
 
         self.laser_projector = laser_geometry.LaserProjection()
         self.tf_listener = tf.TransformListener()
 
+        self.state = "MOVE"  # Default state
+
     def laser_callback(self, data):
-        try:
-            # Transform the laser scan into a point cloud
-            cloud = self.laser_projector.projectLaser(data)
-            # Publish the point cloud
-            self.cloud_pub.publish(cloud)
-        except (tf.Exception, tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            pass
+        # Process and publish the point cloud only if in MAP state
+        if self.state == "MAP":
+            try:
+                cloud = self.laser_projector.projectLaser(data)
+                self.cloud_pub.publish(cloud)
+            except (tf.Exception, tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                pass
+
+    def state_callback(self, msg):
+        # Update the state based on the received message
+        self.state = msg.data
+        if self.state == "MAP":
+            rospy.loginfo("State changed to MAP")
+        elif self.state == "MOVE":
+            rospy.loginfo("State changed to MOVE")
+        else:
+            rospy.loginfo("Invalid state, please enter either MAP or MOVE")
 
 if __name__ == '__main__':
     node = LaserScanToPointCloud()
